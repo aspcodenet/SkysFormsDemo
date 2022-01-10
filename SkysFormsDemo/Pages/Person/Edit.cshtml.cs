cc15867b5fb825d0982728acc1e14bdc104cf65f
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-01-10 11:40:42 +0100

    aaa

--- SkysFormsDemo/Pages/Person/Edit.cshtml.cs
+++ SkysFormsDemo/Pages/Person/Edit.cshtml.cs
@@ -3,15 +3,83 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Net.Http.Headers;
+using SkysFormsDemo.Data;
 using SkysFormsDemo.Services;
 
 namespace SkysFormsDemo.Pages.Person
 {
+    [BindProperties]
     public class EditModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ApplicationDbContext _context;
 
+        [MaxLength(100)]
+        [Required]
+        public string Name { get; set; }
+
+        [StringLength(100)]
+        public string StreetAddress { get; set; }
+
+        [StringLength(10)]
+        public string PostalCode { get; set; }
+
+        [StringLength(2)] public string CountryCode { get; set; }
+
+        [Range(0, 100000, ErrorMessage = "Skriv ett tal mellan 0 och 100000")]
+        public decimal Salary { get; set; }
+
+
+        [Range(0, 100)]
+        public int CarCount { get; set; } //Krysta fram ett int-usecase
+
+        [StringLength(50)]
+        [Required]
+        public string City { get; set; }
+
+        [StringLength(150)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+        public EditModel(ApplicationDbContext context)
+        {
+            _context = context;
         }
+
+        public void OnGet(int personId)
+        {
+            var person = _context.Person.First(u=>u.Id == personId );
+            Name = person.Name;
+            CarCount = person.CarCount;
+            City = person.City;
+            CountryCode = person.CountryCode;
+            Email = person.Email;
+            PostalCode = person.PostalCode;
+            Salary = person.Salary;
+            StreetAddress = person.StreetAddress;
+        }
+
+
+        public IActionResult OnPost(int personId)
+        {
+            if (ModelState.IsValid)
+            {
+                var person = _context.Person.First(u=>u.Id == personId);
+                person.Name = Name;
+                person.CarCount = CarCount;
+                person.City = City;
+                person.CountryCode = CountryCode;
+                person.Email = Email;
+                person.PostalCode = PostalCode;
+                person.Salary = Salary;
+                person.StreetAddress = StreetAddress;
+                _context.SaveChanges();
+                return RedirectToPage("Index");
+            }
+
+            return Page();
+        }
+
+
     }
 }
--- SkysFormsDemo/Pages/Person/New.cshtml.cs
+++ SkysFormsDemo/Pages/Person/New.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Net.Http.Headers;
+using SkysFormsDemo.Data;
 using SkysFormsDemo.Services;
 
 namespace SkysFormsDemo.Pages.Person
@@ -10,9 +11,12 @@ namespace SkysFormsDemo.Pages.Person
     [BindProperties]
     public class NewModel : PageModel
     {
-        [StringLength(100)]
+        private readonly ApplicationDbContext _context;
+
+        [MaxLength(100)]
         [Required]
         public string Name { get; set; }
+        
         [StringLength(100)]
         public string StreetAddress { get; set; }
 
@@ -25,6 +29,7 @@ namespace SkysFormsDemo.Pages.Person
         public decimal Salary { get; set; }
 
 
+        [Range(0, 100)]
         public int CarCount { get; set; } //Krysta fram ett int-usecase
 
         [StringLength(50)]
@@ -32,12 +37,42 @@ namespace SkysFormsDemo.Pages.Person
         public string City { get; set; }
 
         [StringLength(150)]
+        [EmailAddress]
         public string Email { get; set; }
 
+        public NewModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
         public void OnGet()
         {
 
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var person = new Data.Person
+                {
+                    CarCount = CarCount,
+                    StreetAddress = StreetAddress,
+                    Email = Email,
+                    City = City,
+                    CountryCode = CountryCode,
+                    Salary = Salary,
+                    Name = Name,
+                    PostalCode = PostalCode,
+                    LastModified = DateTime.UtcNow,
+                    Registered = DateTime.UtcNow
+                };
+                _context.Person.Add(person);
+                _context.SaveChanges();
+                return RedirectToPage("Index");
+            }
+
+            return Page();
         }
 
     }
