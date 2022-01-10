fbc668fd4e2fb8ee69fabde272b02c01f133f0c3
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-01-10 10:41:11 +0100

    Deposit

--- SkysFormsDemo/Data/Account.cs
+++ SkysFormsDemo/Data/Account.cs
@@ -1,11 +1,15 @@
-﻿using System.Reflection.Metadata;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
 
 namespace SkysFormsDemo.Data;
 
 public class Account
 {
     public int Id { get; set; }
+
+    [StringLength(20)]
     public string AccountNo { get; set; }
 
+    [Required]
     public decimal Balance { get; set; }
 }
