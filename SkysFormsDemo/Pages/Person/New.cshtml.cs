c544fceb01df38cce285bd2c7173d5d7ae84249a
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-01-10 11:18:49 +0100

    newperson

--- SkysFormsDemo/Pages/Person/New.cshtml.cs
+++ SkysFormsDemo/Pages/Person/New.cshtml.cs
@@ -7,8 +7,34 @@ using SkysFormsDemo.Services;
 
 namespace SkysFormsDemo.Pages.Person
 {
+    [BindProperties]
     public class NewModel : PageModel
     {
+        [StringLength(100)]
+        [Required]
+        public string Name { get; set; }
+        [StringLength(100)]
+        public string StreetAddress { get; set; }
+
+        [StringLength(10)]
+        public string PostalCode { get; set; }
+
+        [StringLength(2)] public string CountryCode { get; set; }
+
+        [Range(0, 100000, ErrorMessage = "Skriv ett tal mellan 0 och 100000")]
+        public decimal Salary { get; set; }
+
+
+        public int CarCount { get; set; } //Krysta fram ett int-usecase
+
+        [StringLength(50)]
+        [Required]
+        public string City { get; set; }
+
+        [StringLength(150)]
+        public string Email { get; set; }
+
+
         public void OnGet()
         {
 
